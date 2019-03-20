77e9c99c9d193623fd189d60812d00b7e8ee3aa6
PureDreamCN <33258608+PureDreamCN@users.noreply.github.com>
2019-03-20 15:15:17 +0800

    Update GittestApplication.java

--- src/main/java/com/gf/gittest/GittestApplication.java
+++ src/main/java/com/gf/gittest/GittestApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GittestApplication {
 
     public static void main(String[] args) {
+        System.out.println("你好呀");
         SpringApplication.run(GittestApplication.class, args);
     }
 
